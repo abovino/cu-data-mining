35b3e5fcf47a797e7ed8c6f50cfd1b0d3db67b65
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-23 19:12:52 -0400

    Add MongoDB connection

--- scraper.py
+++ scraper.py
@@ -1,12 +1,31 @@
 from requests import get
 from bs4 import BeautifulSoup
+from datetime import datetime
+from dotenv import load_dotenv
+import os
+import pymongo
 
-res = get('https://www.westmetall.de/en/markdaten.php', stream=True).content
+load_dotenv()
+
+url = 'https://www.westmetall.de/en/markdaten.php'
+
+res = get(url, stream=True).content
 html = BeautifulSoup(res, 'html.parser')
-del_cu_rate = html.findAll('table')[3].findAll('tr')[3].findAll('td')[1].text
-eur_to_usd_ex_rate = html.findAll('table')[2].findAll('tr')[1].findAll('td')[1].text
-camden_cu_rate = 3.03
+del_cu_rate = html.find_all(attrs={'href': '?action=show_diagram&field=DEL_high'})[1].text
+eur_to_usd_ex_rate = html.find_all(attrs={'href': '?action=show_diagram&field=Euro_MTLE'})[1].text
 
-print(del_cu_rate)
-print(eur_to_usd_ex_rate)
-print(camden_cu_rate)
+rates = {
+  'del_cu_rate': del_cu_rate,
+  'eur_to_usd_ex_rate': eur_to_usd_ex_rate,
+  'camden_cu_rate': 3.03, # Freeze at 3.03
+  'date_added': datetime.today()
+}
+
+def push_to_sql(rates):
+  client = pymongo.MongoClient(os.environ['MONGODB_URI'])
+  db = client['cu_data_mining']
+  rates_collection = db['rates']
+  rates_collection.insert_one(rates)
+
+# Push to sql database ...
+push_to_sql(rates)
