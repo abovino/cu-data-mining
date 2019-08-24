2b53a11fdee99ef8c9f3b529949273bdfd9fa81c
Angelo Bovino <angelo.bovino@outlook.com>
2019-08-23 20:42:18 -0400

    Add error logging and mysql support

--- scraper.py
+++ scraper.py
@@ -1,31 +1,44 @@
 from requests import get
 from bs4 import BeautifulSoup
-from datetime import datetime
 from dotenv import load_dotenv
+from datetime import datetime
 import os
-import pymongo
+import mysql.connector
 
 load_dotenv()
 
-url = 'https://www.westmetall.de/en/markdaten.php'
+def get_todays_rates(url):
+  res = get(url, stream=True).content
+  html = BeautifulSoup(res, 'html.parser')
+  del_cu_rate = html.find_all(attrs={'href': '?action=show_diagram&field=DEL_high'})[1].text
+  eur_to_usd_ex_rate = html.find_all(attrs={'href': '?action=show_diagram&field=Euro_MTLE'})[1].text
 
-res = get(url, stream=True).content
-html = BeautifulSoup(res, 'html.parser')
-del_cu_rate = html.find_all(attrs={'href': '?action=show_diagram&field=DEL_high'})[1].text
-eur_to_usd_ex_rate = html.find_all(attrs={'href': '?action=show_diagram&field=Euro_MTLE'})[1].text
-
-rates = {
-  'del_cu_rate': del_cu_rate,
-  'eur_to_usd_ex_rate': eur_to_usd_ex_rate,
-  'camden_cu_rate': 3.03, # Freeze at 3.03
-  'date_added': datetime.today()
-}
-
-def push_to_mongodb(rates):
-  client = pymongo.MongoClient(os.environ['MONGODB_URI'])
-  db = client['cu_data_mining']
-  rates_collection = db['rates']
-  rates_collection.insert_one(rates)
+  return {
+    'del_cu_rate': del_cu_rate,
+    'eur_to_usd_ex_rate': eur_to_usd_ex_rate,
+    'camden_cu_rate': 3.03, # Freeze at 3.03
+  }
 
 
-push_to_mongodb(rates)
+def insert_into_rates(rates):
+  db_conn = mysql.connector.connect(
+    host=os.environ['DB_HOST'],
+    user=os.environ['DB_USER'],
+    passwd=os.environ['DB_PASS'],
+    database=os.environ['DB_DATABASE']
+  )
+
+  cursor = db_conn.cursor()
+  mysql_str = f"""INSERT INTO rates (del_cu_rate, eur_to_usd_ex_rate, camden_cu_rate)
+                 VALUES ({rates["del_cu_rate"]}, {rates["eur_to_usd_ex_rate"]}, {rates["camden_cu_rate"]})"""
+  cursor.execute(mysql_str)
+  db_conn.commit()
+
+
+try:
+  url = 'https://www.westmetall.de/en/markdaten.php'
+  rates = get_todays_rates(url)
+  insert_into_rates(rates)
+except Exception as e:
+  log_fail = open('logs.txt', 'a')
+  log_fail.write(f'[{datetime.today()}] - The following error occured: {str(e)}\n')
